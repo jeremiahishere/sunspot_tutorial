e2114ba9a9dde0054367c90198302c8167ddc368
Jeremiah Hemphill <jeremiah@cloudspace.com>
2013-03-11 16:00:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotTutorial::Application.config.secret_token = '6054a10e6c8b5aad2de97531931490aeeafb15d6d58e0c138812bcce32f3d1717ac5b17d97744ca3519ee25eba7eef73b6a6ce07f5c69580bce337f98a38b4eb'
